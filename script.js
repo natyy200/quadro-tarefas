046a879168029cfe34ac674063f890989ef15d81
Natyy__ <131829677+natyy200@users.noreply.github.com>
2024-12-13 10:17:19 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const columns = document.querySelectorAll('.column__cards');
+const cards = document.querySelectorAll('.card');
+
+cards.forEach((card) => {
+    card.addEventListener('dragstart', () => {
+        
+    });
+});
